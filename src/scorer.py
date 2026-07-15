55d44b7ea484f991304cc7c53b7d7076755f5adf
Jyoti Ranjan Panda <jrpanda.dev@gmail.com>
2026-07-15 21:26:42 +0530

    Add resume scoring algorithm

--- src/scorer.py
+++ src/scorer.py
@@ -1,3 +1,24 @@
-# Placeholder for Scorer
-def calculate_score(match_details: dict) -> int:
-    pass
+def calculate_score(comparison_result, hr):
+    # ----------- Skills Score (60%) -----------
+
+    total_skills = len(hr["skills"])
+    matched_skills = len(comparison_result["matched_skills"])
+
+    skill_score = (matched_skills / total_skills) * 60
+
+    # ----------- Experience Score (20%) -----------
+
+    experience_score = 20 if comparison_result["experience_match"] else 0
+
+    # ----------- Projects Score (20%) -----------
+
+    project_score = 20 if comparison_result["projects_match"] else 0
+
+    total_score = skill_score + experience_score + project_score
+
+    return round(total_score, 2)
+
+    
+    
+
+
